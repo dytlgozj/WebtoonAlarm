bf5ae07e4b02d56b9823108989e85b88c413a527
dytlgozj <dytlgozj@naver.com>
2016-12-01 18:11:11 +0900

    Insert text "go back"
    Insert text "go back"

--- WAP/app/src/main/java/teamwap/wap/SettingsActivity.java
+++ WAP/app/src/main/java/teamwap/wap/SettingsActivity.java
@@ -19,24 +19,16 @@ import android.preference.PreferenceManager;
 import android.preference.RingtonePreference;
 import android.text.TextUtils;
 import android.view.MenuItem;
+import android.widget.Toast;
+
 import java.util.List;
 
-/**
- * A {@link PreferenceActivity} that presents a set of application settings. On
- * handset devices, settings are presented as a single list. On tablets,
- * settings are split by category, with category headers shown to the left of
- * the list of settings.
- * <p>
- * See <a href="http://developer.android.com/design/patterns/settings.html">
- * Android Design: Settings</a> for design guidelines and the <a
- * href="http://developer.android.com/guide/topics/ui/settings.html">Settings
- * API Guide</a> for more information on developing a Settings UI.
- */
 public class SettingsActivity extends AppCompatPreferenceActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()){
             case android.R.id.home:{
+                Toast.makeText(getApplicationContext(), "이전화면으로 돌아갑니다.", Toast.LENGTH_SHORT).show();
                 finish();
                 break;
             }
@@ -46,11 +38,6 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         }
         return super.onOptionsItemSelected(item);
     }
-
-    /**
-     * A preference value change listener that updates the preference's summary
-     * to reflect its new value.
-     */
 
     private static Preference.OnPreferenceChangeListener sBindPreferenceSummaryToValueListener = new Preference.OnPreferenceChangeListener() {
         @Override
