fc05aec7f9dda7e6de55d075c86aaedcd23214f5
Libria <litaparit732@hanyang.ac.kr>
2016-12-07 13:31:14 +0900

    Fail to make xml -> Activity.

---
+++ WAP/app/src/main/java/teamwap/wap/License.java
@@ -0,0 +1,22 @@
+package teamwap.wap;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by Tabi on 2016-12-07.
+ */
+
+public class License extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.mipmap.ic_launcher2);
+    }
+}
