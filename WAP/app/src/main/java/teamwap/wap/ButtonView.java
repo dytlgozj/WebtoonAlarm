c135ae203fd9fd95da687dc92328a11ddc3f852d
dytlgozj <dytlgozj@naver.com>
2016-12-12 21:05:37 +0900

    Refactoring ButtonView.java
    Refactoring ButtonView.java

--- WAP/app/src/main/java/teamwap/wap/ButtonView.java
+++ WAP/app/src/main/java/teamwap/wap/ButtonView.java
@@ -6,54 +6,52 @@ import android.view.*;
 import android.widget.*;
 import android.util.*;
 public ButtonView extends LinearLayout implements View.OnClickListener{
-public void onClick(View v){
-
-}
+    public void onClick(View v){
+    }
 public static class EtcView extends LinearLayout{
   
-public EtcView(Context ctx){//코드내에서만 생성되는 동적 생성자
-super(ctx);
-setOrientation(VERTICAL);
-TextView tv = new TextView(ctx);
-tv.setText("sub text");
-addView(tv);
-}
-public EtcView(Context ctx,AttributeSet attrs){//xml 에서 사용할수있는 생성자 
-super(ctx);
-setOrientation(VERTICAL);
-TextView tv = new TextView(ctx);
-tv.setText("sub text");
-addView(tv);
-}
-
+    public EtcView(Context ctx){//코드내에서만 생성되는 동적 생성자
+        super(ctx);
+        setOrientation(VERTICAL);
+        TextView tv = new TextView(ctx);
+        tv.setText("sub text");
+        addView(tv);
+    }
+    public EtcView(Context ctx,AttributeSet attrs){//xml 에서 사용할수있는 생성자
+        super(ctx);
+        setOrientation(VERTICAL);
+        TextView tv = new TextView(ctx);
+        tv.setText("sub text");
+        addView(tv);
+    }
 }
 
 public ButtonView(Context ctx){//코드내에서만 생성되는 동적 생성자
-super(ctx);
-setOrientation(HORIZONTAL);
-Button btn = new Button(ctx);
-btn.setText("haha");
-btn.setOnClickListener(this);
-addView(btn);
-addView(new EtcView(ctx));
+    super(ctx);
+    setOrientation(HORIZONTAL);
+    Button btn = new Button(ctx);
+    btn.setText("haha");
+    btn.setOnClickListener(this);
+    addView(btn);
+    addView(new EtcView(ctx));
 }
 public ButtonView(Context ctx,AttributeSet attrs){//xml 에서 사용할수있는 생성자 
-super(ctx);
-setOrientation(HORIZONTAL);
-Button btn = new Button(ctx);
-btn.setText("haha");
-btn.setOnClickListener(this);
-addView(btn);
-addView(new EtcView(ctx));
+    super(ctx);
+    setOrientation(HORIZONTAL);
+    Button btn = new Button(ctx);
+    btn.setText("haha");
+    btn.setOnClickListener(this);
+    addView(btn);
+    addView(new EtcView(ctx));
 }
 public ButtonView(Context ctx,AttributeSet attrs,int themeResId){//xml 에서 사용할수있는 생성자이고 android:style 였던가 태그속성을 사용할때 사용하는 생성자 
-super(ctx,attrs,themeResId);
-setOrientation(HORIZONTAL);
-Button btn = new Button(ctx);
-btn.setText("haha");
-btn.setOnClickListener(this);
-addView(btn);
-addView(new EtcView(ctx));
+    super(ctx,attrs,themeResId);
+    setOrientation(HORIZONTAL);
+    Button btn = new Button(ctx);
+    btn.setText("haha");
+    btn.setOnClickListener(this);
+    addView(btn);
+    addView(new EtcView(ctx));
 }
 
 }
